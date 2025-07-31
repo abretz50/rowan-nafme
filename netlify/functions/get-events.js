abd26818c3c619b4f85eef96c7ba5766a7b336e2
abretz50 <abretz50@gmail.com>
2025-07-31 18:42:24 -0400

    Update get-events.js

--- netlify/functions/get-events.js
+++ netlify/functions/get-events.js
@@ -1,1 +1,24 @@
+const { Client } = require('pg');
+
+exports.handler = async function () {
+  const client = new Client({
+    connectionString: process.env.NEON_DB_URL,
+    ssl: { rejectUnauthorized: false }
+  });
+
+  try {
+    await client.connect();
+    const res = await client.query('SELECT * FROM events ORDER BY date ASC');
+    await client.end();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(res.rows),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }),
+    };
+  }
+};
 
